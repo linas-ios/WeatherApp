66c163c2075f125763d254e7f74f3c4af0d2b887
linas-ios <linasn404@gmail.com>
2023-01-28 21:02:59 +0100

    add assets and finish top stack

--- WeatherApp/WeatherViewController.swift
+++ WeatherApp/WeatherViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class WeatherViewController: UIViewController {
 
+  @IBOutlet weak var button: UIButton!
+  @IBOutlet weak var uiview: UIView!
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
 
 
